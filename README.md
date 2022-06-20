f82a01258154a24ad6ebdb5b1c85a36789971778
Marius <marius@example.com>
2022-06-20 10:35:05 +0300

    empty pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-11-formik-customHook-authIntro
+
+1. install react router @^5
+2. / HomePage
+3. /about AboutPage
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,6 @@
+// import css from './HomePage.module.css'
+
+function HomePage() {
+  return <div>HomePage</div>;
+}
+export default HomePage;

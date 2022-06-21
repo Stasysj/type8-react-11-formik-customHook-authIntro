d04277851f045bab664fdcc2a8378e2d4c6a0bf0
Marius <marius@example.com>
2022-06-21 09:51:38 +0300

    touched

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -23,29 +23,37 @@ function LoginForm() {
     },
   });
 
-  console.log('formik.errors ===', formik.errors);
+  console.log('formik.touched ===', formik.touched);
   // console.log('formik.values ===', formik.values);
 
   return (
     <form onSubmit={formik.handleSubmit}>
       <input
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
-        className={formik.errors.email ? css.errorInput : ''}
+        className={formik.touched.email && formik.errors.email ? css.errorInput : ''}
         name='email'
         type='text'
         placeholder='Your email'
       />
-      {formik.errors.email && <p className={css.errorMsg}>{formik.errors.email}</p>}
+      {formik.touched.email && formik.errors.email && (
+        <p className={css.errorMsg}>{formik.errors.email}</p>
+      )}
       <input
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.password}
-        className={formik.errors.password ? css.errorInput : ''}
+        className={
+          formik.touched.password && formik.errors.password ? css.errorInput : ''
+        }
         name='password'
         type='password'
         placeholder='Your password'
       />
-      {formik.errors.password && <p className={css.errorMsg}>{formik.errors.password}</p>}
+      {formik.touched.password && formik.errors.password && (
+        <p className={css.errorMsg}>{formik.errors.password}</p>
+      )}
       <button type='submit'>Login</button>
     </form>
   );

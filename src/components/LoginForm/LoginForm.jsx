a7fa8e5385f64bef2c10d9c9bc23eb66e5600f21
Marius <marius@example.com>
2022-06-21 09:44:51 +0300

    yup validation, all errros

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,6 @@
+import css from './LoginForm.module.css';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const initValues = {
   email: '',
@@ -7,11 +9,21 @@ const initValues = {
 function LoginForm() {
   const formik = useFormik({
     initialValues: initValues,
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Patikrinkite savo email')
+        .required(),
+      password: Yup.string()
+        .min(4, 'Maziausiai 4 simboliai')
+        .max(7)
+        .required(),
+    }),
     onSubmit: (values) => {
       console.log('submiting values ===', values);
     },
   });
 
+  console.log('formik.errors ===', formik.errors);
   // console.log('formik.values ===', formik.values);
 
   return (
@@ -19,17 +31,21 @@ function LoginForm() {
       <input
         onChange={formik.handleChange}
         value={formik.values.email}
+        className={formik.errors.email ? css.errorInput : ''}
         name='email'
         type='text'
         placeholder='Your email'
       />
+      {formik.errors.email && <p className={css.errorMsg}>{formik.errors.email}</p>}
       <input
         onChange={formik.handleChange}
         value={formik.values.password}
+        className={formik.errors.password ? css.errorInput : ''}
         name='password'
         type='password'
         placeholder='Your password'
       />
+      {formik.errors.password && <p className={css.errorMsg}>{formik.errors.password}</p>}
       <button type='submit'>Login</button>
     </form>
   );

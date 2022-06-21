2bb657d2aabca0df0f8b11cac3f831aaaecc311b
Marius <marius@example.com>
2022-06-21 09:22:30 +0300

    2 way binding

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -9,10 +9,24 @@ function LoginForm() {
     initialValues: initValues,
   });
 
+  console.log('formik.values ===', formik.values);
+
   return (
     <form>
-      <input type='text' placeholder='Your email' />
-      <input type='password' placeholder='Your password' />
+      <input
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        name='email'
+        type='text'
+        placeholder='Your email'
+      />
+      <input
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        name='password'
+        type='password'
+        placeholder='Your password'
+      />
       <button type='submit'>Login</button>
     </form>
   );
